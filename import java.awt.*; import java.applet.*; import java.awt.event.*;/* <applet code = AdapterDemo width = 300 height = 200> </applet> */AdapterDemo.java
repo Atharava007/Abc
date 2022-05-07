53859762317fe71eb97cb2a0030014bfa2ae989e
Atharava007 <60058945+Atharava007@users.noreply.github.com>
2022-05-07 13:34:59 +0530

    Create AdapterDemo.java

---
+++ import java.awt.*; import java.applet.*; import java.awt.event.*;/* <applet code = AdapterDemo width = 300 height = 200> </applet> */AdapterDemo.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+/*
+<applet code = AdapterDemo width = 300 height = 200>
+</applet>
+*/
+
+public class AdapterDemo extends Applet
+{
+    public void init()
+    {
+   	 addMouseListener(new MyMouse(this));
+   	 addMouseMotionListener(new MyMouse(this));
+    }
+}
+
