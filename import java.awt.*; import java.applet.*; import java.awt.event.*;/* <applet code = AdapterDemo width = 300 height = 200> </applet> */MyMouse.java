834c6e69129f250b3304893db8fb4779307698fa
Atharava007 <60058945+Atharava007@users.noreply.github.com>
2022-05-07 13:37:23 +0530

    Create MyMouse.java

---
+++ import java.awt.*; import java.applet.*; import java.awt.event.*;/* <applet code = AdapterDemo width = 300 height = 200> </applet> */MyMouse.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+/*
+<applet code = MyMouse width = 300 height = 200>
+</applet>
+*/
+
+public class MyMouse extends MouseAdapter
+{
+    AdapterDemo ad;
+    public MyMouse(AdapterDemo ad)
+    {
+   	 this.ad = ad;
+    }
+    public void mouseDragged(MouseEvent me)
+    {
+   	 ad.showStatus("Dragged");
+    }
+    public void mouseMoved(MouseEvent me)
+    {
+   	 ad.showStatus("Moved");
+    }
+    public void mouseClicked(MouseEvent me)
+    {
+   	 ad.showStatus("Clicked");
+    }
+}
+
